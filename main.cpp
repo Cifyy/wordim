9b306c03c37357c257855ad822283f5226b5928a
Cifyy <108935717+Cifyy@users.noreply.github.com>
2025-06-28 19:19:53 +0200

    text file input

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <set>
 
 using namespace std;
+
+void eliminateMultipleCharacters(vector<string> &lineCandidates, const string &pattern)
+{
+    for (int j = 0; j < lineCandidates.size(); j++)
+    {
+        set<char> uniqueChars;
+        for (int i = 0; i < 5; i++)
+        {
+            if (pattern[i] == '2' || pattern[i] == '1' || pattern[i] == 'X')
+            {
+                if (uniqueChars.find(lineCandidates[j][i]) != uniqueChars.end())
+                {
+                    lineCandidates.erase(lineCandidates.begin() + j);
+                    j--;
+                    break;
+                }
+                uniqueChars.insert(lineCandidates[j][i]);
+            }
+        }
+    }
+}
 
 void filterYellow(vector<string> &lineCandidates, const string &keyword, const string &pattern)
 {
@@ -13,7 +35,18 @@ void filterYellow(vector<string> &lineCandidates, const string &keyword, const s
         {
             for (int j = 0; j < lineCandidates.size(); j++)
             {
-                if (lineCandidates[j][i] == pattern[i] || lineCandidates[j].find(keyword[i]) == string::npos)
+                if (keyword.find(lineCandidates[j][i]) == string::npos || lineCandidates[j][i] == keyword[i])
+                {
+                    lineCandidates.erase(lineCandidates.begin() + j);
+                    j--;
+                }
+            }
+        }
+        else if (pattern[i] == 'X')
+        {
+            for (int j = 0; j < lineCandidates.size(); j++)
+            {
+                if (keyword.find(lineCandidates[j][i]) == string::npos)
                 {
                     lineCandidates.erase(lineCandidates.begin() + j);
                     j--;
@@ -31,7 +64,7 @@ void filterGray(vector<string> &lineCandidates, const string &keyword, const str
         {
             for (int j = 0; j < lineCandidates.size(); j++)
             {
-                if (keyword.find(lineCandidates[i][j]) != string::npos)
+                if (keyword.find(lineCandidates[j][i]) != string::npos)
                 {
                     lineCandidates.erase(lineCandidates.begin() + j);
                     j--;
@@ -50,6 +83,7 @@ void prePopulateGreen(vector<string> &lineCandidates, const vector<string> (&wor
             lineCandidates.insert(lineCandidates.end(), words[i][keyword[i] - 'a'].begin(), words[i][keyword[i] - 'a'].end());
             break;
         }
+    i++;
 
     for (i; i < 5; i++)
     {
@@ -67,9 +101,9 @@ void prePopulateGreen(vector<string> &lineCandidates, const vector<string> (&wor
     }
 }
 
-void addCandidates(vector<string> (&candidates)[6], const vector<string> (&words)[5][26], const string &keyword, const string pattern[6])
+void addCandidates(vector<string> (&candidates)[6], const vector<string> (&words)[5][26], const string &keyword, const string pattern[6], const bool only_unique = false)
 {
-
+    cout << "0/6" << endl;
     for (int i = 0; i < 6; i++)
     {
         prePopulateGreen(candidates[i], words, keyword, pattern[i]);
@@ -81,7 +115,14 @@ void addCandidates(vector<string> (&candidates)[6], const vector<string> (&words
 
         filterYellow(candidates[i], keyword, pattern[i]);
         filterGray(candidates[i], keyword, pattern[i]);
+
+        if (only_unique)
+            eliminateMultipleCharacters(candidates[i], pattern[i]);
+
+        system("cls");
+        cout << i + 1 << "/6" << endl;
     }
+    system("cls");
 }
 
 int main()
@@ -100,12 +141,14 @@ int main()
     infile.close();
 
     string keyword;
+    cout << "Enter the keyword: ";
     cin >> keyword;
     if (keyword.length() != 5)
     {
         cerr << "Input must be 5 characters long." << endl;
         return 1;
     }
+    system("cls");
     // 0 = gray, 1 = yellow, 2 = green, X = either green or yellow
     string pattern[6] = {
         "21112",
@@ -115,20 +158,28 @@ int main()
         "10001",
         "22222"};
     vector<string> candidates[6];
-    addCandidates(candidates, words, keyword, pattern);
+    addCandidates(candidates, words, keyword, pattern, only_unique);
 
     for (int i = 0; i < 6; i++)
     {
-        cout << "Candidates for pattern " << pattern[i] << ":" << endl;
-        int count = 0;
-        for (const auto &candidate : candidates[i])
+        // cout << candidates[i].size() << " candidates found for pattern " << pattern[i] << ":" << endl;
+        // int count = 0;
+        // for (const auto &candidate : candidates[i])
+        // {
+        // count++;
+        // if (count > 10)
+        // break;
+        // cout << candidate << endl;
+        // }
+        // cout << endl;
+        if (candidates[i].empty())
         {
-            count++;
-            if (count > 10)
-                break;
-            cout << candidate << endl;
+            cout << "No candidates found for pattern " << pattern[i] << "." << endl;
         }
-        cout << endl;
+        else
+        {
+            cout << i + 1 << ". " << candidates[i][0] << endl;
+        }
     }
 
     return 0;
